b52889cdf3edbc9157c52edd4bb7fc095095e692
Avrumi Friedman <litw13gh@gmail.com>
2026-03-25 22:22:20 -0400

    homework instructions

--- ListPostDemo.Web/Controllers/HomeController.cs
+++ ListPostDemo.Web/Controllers/HomeController.cs
@@ -23,3 +23,16 @@ namespace ListPostDemo.Web.Controllers
 
     }
 }
+
+//create an application that displays a list of people. On top of the list
+//have a link that takes you to a page that allows the user to enter multiple
+//people. When that page loads, there should be one row of textboxes (first name,
+//lastname and age) with a button on top that says "Add". When Add is clicked,
+//another row of textboxes should appear. Beneath those textboxes, there should
+//be a submit button, that when clicked, adds all those people to the database
+//and then redirects the user back to the home page (with a success message showing on top).
+//On the home page, in the first
+//column of every row, have a checkbox. On top of that column (in the header) have a
+//"Delete all" button. When this button is clicked, all the people that were checked
+//off, should get deleted. The user should get redirected back to the home page, and
+//a message should be displayed on top.
